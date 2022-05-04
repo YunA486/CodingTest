da78561c6a93a503f7f87fa1708feecf3132e4e6
YunA486 <w2039@e-mirim.hs.kr>
2022-05-04 23:08:55 +0900

    2022.05.04 - 수박수박수박수박수박수 - 다른 사람의 풀이

---
+++ 20220504_수박수박수박수박수박수.py
@@ -0,0 +1,21 @@
+# 길이가 n이고, "수박수박수박수...."와 같은 패턴을 유지하는 문자열을 리턴하는 함수, solution을 완성하세요.
+# 예를들어 n이 4이면 "수박수박"을 리턴하고 3이라면 "수박수"를 리턴하면 됩니다.
+
+def solution(n):
+    answer = ''
+    for i in range(1, n+1):
+        if i % 2 == 1:
+            answer += '수'
+        else:
+            answer += '박'
+    return answer
+
+# 다른 사람의 풀이
+def water_melon(n):
+    s = "수박" * n
+    return s[:n]
+
+
+# 실행을 위한 테스트코드입니다.
+print("n이 3인 경우: " + water_melon(3))
+print("n이 4인 경우: " + water_melon(4))
