51d75d648597b4b3a121946237e2e8bddf0204ba
YunA486 <w2039@e-mirim.hs.kr>
2022-05-05 22:02:14 +0900

    2022.05.04 - 문자열을 정수로 바꾸기 - 다른 사람의 풀이

---
+++ 20220505_문자열을정수로바꾸기.py
@@ -0,0 +1,13 @@
+# 문자열 s를 숫자로 변환한 결과를 반환하는 함수, solution을 완성하세요.
+def solution(s):
+    return int(s)
+
+# 다른 사람의 풀이
+def strToInt(str):
+    #함수를 완성하세요
+    a = int(str)
+    return a
+
+
+# 아래는 테스트로 출력해 보기 위한 코드입니다.
+print(strToInt("-1234"))
