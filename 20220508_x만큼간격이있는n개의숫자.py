e7b91a0a6277be8e6d7bd1ef4e0d8ac9af30e663
YunA486 <w2039@e-mirim.hs.kr>
2022-05-08 23:18:06 +0900

    2022.05.08 - x만큼 간격이 있는 n개의 숫자 - 다른 사람의 풀이

---
+++ 20220508_x만큼간격이있는n개의숫자.py
@@ -0,0 +1,18 @@
+# 함수 solution은 정수 x와 자연수 n을 입력 받아, x부터 시작해 x씩 증가하는 숫자를 n개 지니는 리스트를 리턴해야 합니다.
+# 다음 제한 조건을 보고, 조건을 만족하는 함수, solution을 완성해주세요.
+
+def solution(x, n):
+    answer = []
+    i = x
+    while True:
+        answer.append(i)
+        i += x
+        if len(answer) == n:
+            break
+    return answer
+
+# 다른 사람의 풀이
+def number_generator(x, n):
+    # 함수를 완성하세요
+    return [i * x + x for i in range(n)]
+print(number_generator(2, 5))
