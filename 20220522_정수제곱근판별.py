39189a1eba1a8fc6ab9e1a16d53e5eb6355e1641
YunA486 <w2039@e-mirim.hs.kr>
2022-05-22 22:23:51 +0900

    2022.05.22 - 정수 제곱근 판별 - 다른 사람의 풀이

---
+++ 20220522_정수제곱근판별.py
@@ -0,0 +1,21 @@
+# 임의의 양의 정수 n에 대해, n이 어떤 양의 정수 x의 제곱인지 아닌지 판단하려 합니다.
+# n이 양의 정수 x의 제곱이라면 x+1의 제곱을 리턴하고, n이 양의 정수 x의 제곱이 아니라면 -1을 리턴하는 함수를 완성하세요.
+
+import math
+
+def solution(n):
+    temp = n ** (1/2)
+    answer = math.pow(temp+1, 2)
+    if temp.is_integer():
+        return answer
+    else:
+        return -1
+
+# 다른 사람의 풀이
+
+def nextSqure(n):
+    sqrt = n ** (1/2)
+
+    if sqrt % 1 == 0:
+        return (sqrt + 1) ** 2
+    return 'no'
