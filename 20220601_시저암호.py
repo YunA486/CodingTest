1a91a3af3d4484314b05b9c08113c666d758fb9a
YunA486 <w2039@e-mirim.hs.kr>
2022-06-01 20:52:20 +0900

    2022.06.01 - 시저 암호 - 다른 사람의 풀이

---
+++ 20220601_시저암호.py
@@ -0,0 +1,32 @@
+# 어떤 문장의 각 알파벳을 일정한 거리만큼 밀어서 다른 알파벳으로 바꾸는 암호화 방식을 시저 암호라고 합니다.
+# 예를 들어 "AB"는 1만큼 밀면 "BC"가 되고, 3만큼 밀면 "DE"가 됩니다.
+# "z"는 1만큼 밀면 "a"가 됩니다.
+# 문자열 s와 거리 n을 입력받아 s를 n만큼 민 암호문을 만드는 함수, solution을 완성해 보세요.
+
+def solution(s, n):
+    s = list(s)
+    for i in range(len(s)):
+        if s[i].isupper():
+            s[i] = chr((ord(s[i]) - ord('A') + n) % 26 + ord('A'))
+        elif s[i].islower():
+            s[i] = chr((ord(s[i]) - ord('a') + n) % 26 + ord('a'))
+
+    return "".join(s)
+
+# 다른 사람의 풀이
+def caesar(s, n):
+    lower_list = "abcdefghijklmnopqrstuvwxyz"
+    upper_list = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
+    result = []
+
+    for i in s:
+        if i is " ":
+            result.append(" ")
+        elif i.islower() is True:
+            new_ = lower_list.find(i) + n
+            result.append(lower_list[new_ % 26])
+        else:
+            new_ = upper_list.find(i) + n
+            result.append(upper_list[new_ % 26])
+    return "".join(result)
